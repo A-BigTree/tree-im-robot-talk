a95d92b4a0feb2d53374a96e204556fc80c0f959
Shuxin Wang <shuxinwang662@gmail.com>
2024-10-19 20:24:14 +0800

    Create MysqlBaseDO.java

---
+++ tree-im-robot-talk-service/src/main/java/top/abigtree/im/robot/service/entity/db/MysqlBaseDO.java
@@ -0,0 +1,23 @@
+package top.abigtree.im.robot.service.entity.db;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author Shuxin Wang <shuxinwang662@gmail.com>
+ * Created on 2024/10/19
+ */
+@Data
+public class MysqlBaseDO implements Serializable {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    private Long createTime;
+
+    @TableLogic
+    private Integer deleted;
+}
